31a025effeecb146d1bd86be45aa926ce61349f1
Gurden <gurden@darkmatterlabs.org>
2025-02-28 15:53:26 +0530

    remove styles from template, add OrgSearch component

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
+import OrgSearch from "@/components/OrgSearch";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="w-96 mx-auto mt-10">
+      <main className="">
+        <OrgSearch />
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      </footer>
+      <footer className=""></footer>
     </div>
   );
 }
